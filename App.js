137a41c9c3b26f91d00d37fc8f01163dee787698
kalyan ghosh <kalyanghoshguddu@gmail.com>
2025-08-16 15:12:34 +0530

    day 2 ended

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-
-let i=React.createElement("h1",{id:"h1"},"hello react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+let i=React.createElement("h1",{id:"h1"},"hello react avbgtgfh");
 let root=ReactDOM.createRoot(document.querySelector("#root"));
 root.render(i);
 
