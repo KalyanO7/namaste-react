722b14b2b239fbaeb2aa528ae0258f8e416823a5
kalyan ghosh <kalyanghoshguddu@gmail.com>
2025-08-18 14:07:12 +0530

    day 3

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const Head=()=>(
+    <>
+        <App></App>;
+         <h1>hello world</h1>;
+    </>
+)
+
+
 let i=React.createElement("h1",{id:"h1"},"hello react avbgtgfh");
 let root=ReactDOM.createRoot(document.querySelector("#root"));
-root.render(i);
-
+root.render(<Head></Head>);
+const App = () => (
+    <h1>app is running</h1>
+)
